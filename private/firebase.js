a3572a397b2821ce7b5c686563ac7b42dc784a1d
Paraldos <hillbrecht.s@gmail.com>
2024-03-09 09:37:13 +0100

    setup firebase db

---
+++ private/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMLVT4Lj001uLR_50Jrb7GZLYja5y791E",
+  authDomain: "shoppinglist-b16ff.firebaseapp.com",
+  databaseURL:
+    "https://shoppinglist-b16ff-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "shoppinglist-b16ff",
+  storageBucket: "shoppinglist-b16ff.appspot.com",
+  messagingSenderId: "222077516885",
+  appId: "1:222077516885:web:b06e1506c99a4d8c4868dc",
+  measurementId: "G-Z1GJXQTTN0",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db };
--- private/index.js
+++ private/index.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import Navbar from "./components/navbar";
+import { db } from "./firebase.js";
+import { ref, get } from "firebase/database";
 
 const App = () => {
+  useEffect(() => {
+    const testRef = ref(db, "test");
+    get(testRef).then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    });
+  }, []);
+
   return (
     <div>
       <Navbar />
+      <p>Hello World!</p>
     </div>
   );
 };
