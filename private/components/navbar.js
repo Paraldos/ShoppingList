1c59cc2d5844f636e021a6e4f7d043da08435985
Paraldos <hillbrecht.s@gmail.com>
2024-03-09 09:07:03 +0100

    installed font awesome

--- private/components/navbar.js
+++ private/components/navbar.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar__nav-element">01</div>
-      <div className="navbar__nav-element">02</div>
-      <div className="navbar__nav-element">03</div>
+      <div className="navbar__nav-element">
+        <FontAwesomeIcon icon={faHome} />
+      </div>
+      <div className="navbar__nav-element">
+        <FontAwesomeIcon icon={faHome} />
+      </div>
+      <div className="navbar__nav-element">
+        <FontAwesomeIcon icon={faHome} />
+      </div>
     </nav>
   );
 };
