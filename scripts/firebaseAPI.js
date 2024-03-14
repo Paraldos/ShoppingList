8a63ccb29596f008744d6eab0bcca5544a94db4a
Paraldos <hillbrecht.s@gmail.com>
2024-03-14 09:31:07 +0100

    added firebase api

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,62 @@
+import { db } from "./firebase.js";
+import { ref, set, push, update } from "firebase/database";
+
+class FirebaseAPI {
+  constructor() {
+    this.currentUser = null;
+  }
+
+  setCurrentUser(user) {
+    if (!user) return false;
+    this.currentUser = user;
+    window.dispatchEvent(new Event("userIsSet"));
+  }
+
+  getCurrentUser() {
+    return this.currentUser;
+  }
+
+  async addUser(uid, displayName) {
+    await set(ref(db, `users/${uid}`), {
+      userId: uid,
+      displayName: displayName,
+      subscribedLists: [],
+    });
+  }
+
+  async addList(createdBy) {
+    const newListRef = push(ref(db, "lists"));
+    await set(newListRef, {
+      listName: "New List",
+      createdBy: createdBy,
+      subscribers: {
+        [createdBy]: true,
+      },
+    });
+    this.subscribeToList(createdBy, newListRef.key);
+    return newListRef.key;
+  }
+
+  async getSubscribedLists() {
+    const userRef = ref(db, `users/${this.currentUser.uid}/subscribedLists`);
+    console.log(this.currentUser);
+  }
+
+  async subscribeToList(userId, listId) {
+    const userRef = ref(db, `users/${userId}/subscribedLists`);
+    await update(userRef, { [listId]: true });
+  }
+
+  async addItemToList(listId, description, amount) {
+    const newItemRef = push(ref(db, `lists/${listId}/items`));
+    await set(newItemRef, {
+      description,
+      amount,
+      status: "not done",
+    });
+    return newItemRef.key;
+  }
+}
+
+const firebaseAPI = new FirebaseAPI();
+export default firebaseAPI;
