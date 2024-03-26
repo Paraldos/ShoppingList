87dedf391bec0cc554c46a851c5b24ccf4d3fb1f
Paraldos <hillbrecht.s@gmail.com>
2024-03-26 13:20:09 +0100

    added add list btn to navbar

--- scripts/nav.js
+++ scripts/nav.js
@@ -3,6 +3,9 @@ import firebaseAPI from "./firebaseAPI.js";
 export default class Nav {
   constructor() {
     this.loginBtn = document.getElementById("login-btn");
+    this.addListBtn = document.getElementById("add-list-btn");
+
     this.loginBtn.addEventListener("click", () => firebaseAPI.login());
+    this.addListBtn.addEventListener("click", () => firebaseAPI.newList());
   }
 }
