909407f7e606d6cdae015c0a44ee1500274bdfdd
Paraldos <hillbrecht.s@gmail.com>
2024-05-01 14:00:50 +0200

    modal

--- scripts/deletListModal.js
+++ scripts/deletListModal.js
@@ -3,5 +3,7 @@ import Modal from "./modal.js";
 export default class DeleteListModal extends Modal {
   constructor(listId) {
     super();
+    this.modalBody.innerHTML += `
+      <h2>Delete List</h2>`;
   }
 }
--- scripts/modal.js
+++ scripts/modal.js
@@ -1,13 +1,23 @@
 export default class Modal {
-  constructor() {
+  constructor(xButton = false, canBeEscaped = true) {
+    this.canBeEscaped = canBeEscaped;
     this.overlay = this.addOverlay();
     this.modalBody = this.addModalBody();
+    if (xButton) this.addXButton();
   }
 
   addOverlay() {
     const overlay = document.createElement("div");
     overlay.classList.add("modal__overlay");
     document.body.appendChild(overlay);
+    if (this.canBeEscaped) {
+      overlay.addEventListener("click", (event) => {
+        if (event.target === overlay) this.removeModal();
+      });
+      document.addEventListener("keydown", (event) => {
+        if (event.key === "Escape") this.removeModal();
+      });
+    }
     return overlay;
   }
 
@@ -16,6 +26,14 @@ export default class Modal {
     modalBody.classList.add("modal__body");
     this.overlay.appendChild(modalBody);
     return modalBody;
+  }
+
+  addXButton() {
+    const xButton = document.createElement("button");
+    xButton.classList.add("modal__x-button");
+    xButton.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+    xButton.addEventListener("click", () => this.removeModal());
+    this.modalBody.appendChild(xButton);
   }
 
   removeModal() {
