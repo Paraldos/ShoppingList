d95d34bff7e1880d5d4e14c22f1b6c799705d7b1
Paraldos <hillbrecht.s@gmail.com>
2024-07-18 18:25:42 +0200

    removed old material

--- scripts/QRModal.js
+++
@@ -1,9 +0,0 @@
-import Database from "./Database.js";
-import Modal from "./modal.js";
-import SVG from "./svg.js";
-
-export default class QRCodeModal extends Modal {
-  constructor(listId) {
-    super(true);
-  }
-}
