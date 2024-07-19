38e80458b1801c8e113ba9eb877a7dc738edb4c5
Paraldos <hillbrecht.s@gmail.com>
2024-07-19 20:31:52 +0200

    installed tailwind

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import PrimeVue from "primevue/config";
+import "./assets/tailwind.css";
 
 const app = createApp(App);
-
-app.use(PrimeVue);
 
 app.mount("#app");
